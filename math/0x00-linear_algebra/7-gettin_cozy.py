725f994e31be1bcc9d9b2759423951d618964842
juan8bits <juan160820gmail.com>
2021-07-15 16:25:27 -0500

    Move deepcopy to cat_matrices2D function

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -7,29 +7,22 @@
 """
 
 
-def deepcopy(matrix):
-    """
-        Function that make a deep copy to matrix
-        Return: New matrix
-    """
-    return [row[:] for row in matrix]
-
-
 def cat_matrices2D(mat1, mat2, axis=0):
     """
         Function that concatenates two 2D matrices along specific axis
         Return: New matrix
     """
-    if len(mat1) == 0 and len(mat2) == 0 or\
-            len(mat1[0]) == 0 and len(mat2[0]) == 0:
+    if (len(mat1) == 0 and len(mat2) == 0) or\
+            (len(mat1[0]) == 0 and len(mat2[0]) == 0):
         return None
-    conc_result = deepcopy(mat1)
+    # make a deep copy
+    conc_result = [row[:] for row in mat1]
     if axis == 0 and len(mat1[0]) == len(mat2[0]):
         for row in mat2:
             conc_result.append(row)
     elif axis == 1 and len(mat1) == len(mat2):
-        for col in range(len(conc_result)):
-            conc_result[col] += list(mat2[col])
+        for col in range(len(mat2)):
+            conc_result[col] += mat2[col]
     else:
         return None
     return conc_result

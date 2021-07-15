00311090be0886199a4b44ed5050c7d57d3f6f60
juan8bits <juan160820gmail.com>
2021-07-15 12:40:51 -0500

    Make the code more easy and add module documentation

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
-
-
-def matrix_shape(matrix):
-    """
-        Function that calculates the shape of a matrix given in
-        matrix variable.
-
-        Return: List of integers that represent the matrix shape
-    """
-    shape = []
-    dimention = matrix
-    while (isinstance(dimention, list)):
-        shape.append(len(dimention))
-        dimention = dimention[0]
-    return shape
+""" Linear Algebra Module
+    Functions:
+        add_arrays
+"""
 
 
 def add_arrays(arr1, arr2):
@@ -24,10 +13,8 @@ def add_arrays(arr1, arr2):
         Return: New list (array) with sum result.
     """
     sum_result = []
-    if (matrix_shape(arr1) == matrix_shape(arr2)):
-        shape = matrix_shape(arr1)
-        for i in range(shape[0]):
+    if (arr1 and arr2 and len(arr1) == len(arr2)):
+        for i in range(len(arr1)):
             sum_result.append(arr1[i] + arr2[i])
-    else:
-        return None
-    return sum_result
+        return sum_result
+    return None

ee8113fbb863d5e713545f4c6d1dafadeb5b25bb
juan8bits <juan160820gmail.com>
2021-07-15 12:00:01 -0500

    Add module documentation, Add function by import method and add conditions to none input values

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
+""" Linear Algebra module
+    Functions:
+        add_matrices2D
+"""
 
-
-def matrix_shape(matrix):
-    """
-        Function that calculates the shape of a matrix given in
-        matrix variable.
-
-        Return: List of integers that represent the matrix shape
-    """
-    shape = []
-    dimention = matrix
-    while (isinstance(dimention, list)):
-        shape.append(len(dimention))
-        dimention = dimention[0]
-    return shape
+matrix_shape = __import__('2-size_me_please').matrix_shape
 
 
 def add_matrices2D(mat1, mat2):
@@ -23,7 +14,7 @@ def add_matrices2D(mat1, mat2):
         Return: New matrix with sum result.
     """
     sum_result = []
-    if (matrix_shape(mat1) == matrix_shape(mat2)):
+    if (matrix_shape(mat1) == matrix_shape(mat2) and mat1 and mat2):
         shape = matrix_shape(mat1)
         for row in range(shape[0]):
             empty_row = []

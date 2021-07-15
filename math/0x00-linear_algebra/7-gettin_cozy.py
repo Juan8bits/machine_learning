a72a1844c315556cd002fee0286e03573b247df7
juan8bits <juan160820gmail.com>
2021-07-15 15:41:24 -0500

    Add Module documentation and my own deepcopy function

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
-import copy
+"""
+    Linear Algebra Module
+    Functions:
+        deepcopy
+        cat_matrices2D
+"""
+
+
+def deepcopy(matrix):
+    """
+        Function that make a deep copy to matrix
+        Return: New matrix
+    """
+    new_matrix = [row[:] for row in matrix]
+    return new_matrix
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """
         Function that concatenates two 2D matrices along specific axis
-
         Return: New matrix
     """
-    conc_result = copy.deepcopy(mat1)
+    if len(mat1) == 0 and len(mat2) == 0 or\
+            len(mat1[0]) == 0 and len(mat2[0]) == 0:
+        return None
+    conc_result = deepcopy(mat1)
     for i in range(0, 2):
         if i == axis and axis == 0:
             conc_result += mat2

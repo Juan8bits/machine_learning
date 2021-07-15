95c58e4009129f5724ac72aaa9fd5f4f1d0207e7
juan8bits <juan160820gmail.com>
2021-07-15 16:05:19 -0500

    Add new edge cases for simetric matrices

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -24,14 +24,12 @@ def cat_matrices2D(mat1, mat2, axis=0):
             len(mat1[0]) == 0 and len(mat2[0]) == 0:
         return None
     conc_result = deepcopy(mat1)
-    for i in range(0, 2):
-        if i == axis and axis == 0:
-            for row in mat2:
-                conc_result.append(row)
-        elif (i == axis and axis == 1):
-            for col in range(len(conc_result)):
-                conc_result[col] += list(mat2[col])
-        pass
-    if (conc_result == mat1):
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
+        for row in mat2:
+            conc_result.append(row)
+    elif axis == 1 and len(mat1) == len(mat2):
+        for col in range(len(conc_result)):
+            conc_result[col] += list(mat2[col])
+    else:
         return None
     return conc_result

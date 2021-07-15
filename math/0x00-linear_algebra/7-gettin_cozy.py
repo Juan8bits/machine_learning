49c4bb67d2a5145d2cc158551876e210c9425cf1
juan8bits <juan160820gmail.com>
2021-07-15 16:38:46 -0500

    Move deep copy function to inline cod

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -20,15 +20,9 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 0 and len(mat1[0]) == len(mat2[0]):
         for row in mat2:
             conc_result.append(row)
-<<<<<<< HEAD
     elif axis == 1 and len(mat1) == len(mat2):
         for col in range(len(mat2)):
             conc_result[col] += mat2[col]
-=======
-    elif axis == 1:
-        for col in range(len(conc_result)):
-            conc_result[col] += list(mat2[col])
->>>>>>> c711f4e34743b00c8141520bebc922a1b63f15e1
     else:
         return None
     return conc_result

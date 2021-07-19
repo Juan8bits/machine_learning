5336aa78feaa4366bb13c34d262c93b8a706cd41
juan8bits <juan160820gmail.com>
2021-07-18 19:27:41 -0500

    Add advanced task #15

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Linear Algebra Module
+    Functions:
+        no_slice
+"""
+
+
+def np_slice(matrix, axes={}):
+    """ Function that slices a matrix along specific axes.
+
+        Parameters:
+            axes: Is a dictionary where the key is an axis
+                to slice along and the value is a tuple representing
+                the slice to make along that axis.
+            matrix: numpy.ndarray.
+        Return:
+            New numpy.ndarray sliced
+    """
+    slices = []
+    for axs in range(len(matrix)):
+        if axs in axes.keys():
+            value = axes.get(axs)
+            slices.append(slice(*value))
+        else:
+            slices.append(slice(None))
+    return matrix[tuple(slices)]

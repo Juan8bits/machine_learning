dbc913315885072603bd3560acc76d221aad8040
juan8bits <juan160820gmail.com>
2021-07-22 13:47:39 -0500

    2/3 code task, Create file 9 and 10

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Calculus module
+"""
+
+
+def poly_derivative(poly):
+    """ Function that
+
+    Args:
+        poly (List): List of coefficients representing a polynomial.
+                    The index of the lst represents the power of x
+                    that the coeffucient belongs
+    Return:
+        New list of coefficients representing the derivate of the polynomial.
+        if poly is not valid return None
+    """
+    if isinstance(poly, list) is False:
+        return None
+    if len(poly) == 1:
+        return [0]
+    derivate = []
+    for i in range(1, len(poly)):
+        if poly[i] == 0:
+            derivate.append(0)
+        elif i == 1:
+            derivate.append(poly[i])
+        else:
+            derivate.append(i * poly[i])
+    return derivate
---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Calculus module
+    Functions:
+        summation_i_squared(n)
+"""
+
+
+def summation_i_squared(n):
+    """ Write a function that calculates a summation
+        where n is the stopping condition.
+
+        Parameters:
+            n - Stopping summation condition
+        Return:
+            Integer value of the sum, if n is not a valid
+            number return None.
+    """
+    if isinstance(n, int) is False:
+        return None
+
+    initial_condition = 1
+    summatory = 0
+    for i in range(initial_condition, n + 1):
+        summatory += i**2
+    return summatory

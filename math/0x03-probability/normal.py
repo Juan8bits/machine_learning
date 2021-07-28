fb5d91e8e46904cd43287300be2b6229dd66b63b
juan8bits <juan160820gmail.com>
2021-07-28 17:29:48 -0500

    Add pdf calculus

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -6,6 +6,8 @@
 class Normal:
     """ Class to represents a normal distribution.
     """
+    pi = 3.1415926536
+    e = 2.7182818285
 
     def __init__(self, data=None, mean=0., stddev=1.):
         """ Constructor
@@ -48,3 +50,13 @@ class Normal:
             z (int): z value.
         """
         return (z * self.stddev) + self.mean
+
+    def pdf(self, x):
+        """ Method that calculates the value of the PDF for a
+            given x-value
+        Args:
+            x (int): x value.
+        """
+        part_1 = 1 / (self.stddev * ((2 * self.pi) ** (1/2)))
+        exponent = -(1 / 2) * (((x - self.mean) / self.stddev) ** 2)
+        return part_1 * self.e ** exponent

08e054128239d1204905674fdb6d5fa1bb7845af
juan8bits <juan160820gmail.com>
2021-07-28 21:16:31 -0500

    Add task 10

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Class to represents a Binomial distribution.
+"""
+
+
+class Binomial:
+    """ Class to represents a Binomial distribution.
+    """
+    def __init__(self, data=None, n=1, p=0.5):
+        """ Constructor
+
+        Args:
+            data (List): List of the data to be used to estimate
+                the distribution. Defaults to None.
+            n (int): The number of Bernoulli trials. Defaults to 1.
+            p (float):  The probability of a “success”. Defaults to 0.5.
+        """
+        if data is None:
+            if n < 1:
+                raise ValueError('n must be a positive value')
+            if not p > 0 and not p < 1:
+                raise ValueError('p must be greater than 0 and less than 1')
+            self.n = int(n)
+            self.p = float(p)
+        elif not isinstance(data, list):
+            raise TypeError('data must be a list')
+        elif not len(data) > 1:
+            raise ValueError('data must contain multiple values')
+        else:
+            mean = sum(data)/len(data)
+            sum_ = 0
+            for i in data:
+                sum_ += (i - mean)**2
+            variance = sum_/len(data)
+            q = variance/mean
+            self.p = 1 - q
+            self.n = round(mean / self.p)
+            self.p = mean / self.n

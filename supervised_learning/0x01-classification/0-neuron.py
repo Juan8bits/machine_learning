c4cee75751f915add768e649e34e3d4ed4ec1f26
juan8bits <juan160820gmail.com>
2021-08-06 16:08:23 -0500

    change escalar for normal distribution creation

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -21,6 +21,6 @@ class Neuron:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        self.W = np.random.normal(0, 10, (1, nx))
+        self.W = np.random.normal(0, 1, (1, nx))
         self.b = 0
         self.A = 0

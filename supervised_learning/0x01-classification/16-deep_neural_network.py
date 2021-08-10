eae86418e5b529d3af5dd343bfd797c516ac58e0
juan8bits <juan160820gmail.com>
2021-08-10 13:03:11 -0500

    Add deepneuralnetwork class

---
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""  Neural Network Class
+"""
+from typing import Type
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Class that defines a deep neural network performing
+        binary classification.
+    """
+    def __init__(self, nx, layers):
+        """ Constructor method that instanciate a new
+            DeepNeuralNetwork object.
+
+        Args:
+            nx (int): The number of input features.
+            layers (list): List representing the number of
+                nodes in each layer of the network.
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(layers, list) or not layers:
+            raise TypeError('layers must be a list of positive integers')
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for lay in range(self.L):
+            if not isinstance(layers[lay], int) or layers[lay] <= 0:
+                raise TypeError("layers must be a list of positive integers")
+            if lay == 0:
+                He = (np.random.randn(layers[lay], nx)
+                      * np.sqrt(2 / nx))
+                self.weights["W{}".format(lay + 1)] = He
+            else:
+                He = (np.random.randn(layers[lay], layers[lay - 1])
+                      * np.sqrt(2 / layers[lay - 1]))
+                self.weights["W{}".format(lay + 1)] = He
+            # Zero initialization for biases
+            self.weights["b{}".format(lay + 1)] = np.zeros((layers[lay], 1))

84a86e1e2750ed989ff1528c15c89a5d4e931952
juan8bits <juan160820gmail.com>
2021-08-23 22:17:44 -0500

    Add task 0 and empty readme

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """ Function That creates a confusion matrix.
+
+    Args:
+        labels (numpy object): A one-hot numpy.ndarray of shape
+            (m, classes) containing the correct labels for each
+            data point.
+            m: is the number of data points.
+            classes:  is the number of classes.
+        logits (numpy object): A one-hot numpy.ndarray of shape
+            (m, classes) containing the predicted labels.
+
+    Returns:
+        A confusion numpy.ndarray of shape (classes, classes)
+        with row indices representing the correct labels and
+        column indices representing the predicted labels.
+    """
+    c_matrix = np.matmul(labels.T, logits)
+
+    return c_matrix

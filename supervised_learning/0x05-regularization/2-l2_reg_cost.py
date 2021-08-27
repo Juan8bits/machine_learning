0f54bb694fba977ad7a175497cc1efe296b88477
juan8bits <juan160820gmail.com>
2021-08-26 22:09:19 -0500

    add task 2

---
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Gradient Descent with L2 Regularization
+"""
+import tensorflow as tf
+
+
+def l2_reg_cost(cost):
+    """ Function that calculates the cost of a neural network
+        with L2 regularization.
+
+    Args:
+        cost (td object): Tensor containing the cost of the
+            network without L2 regularization.
+    Return:
+        A tensor containing the cost of the network accounting
+        for L2 regularization.
+    """
+    l2_cost = tf.losses.get_regularization_losses(scope=None)
+    return cost + l2_cost

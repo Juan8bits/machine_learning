472f5794739ac5ffadd3a08b174f470da00fe16b
juan8bits <juan160820gmail.com>
2021-09-04 12:52:50 -0500

    Add task 7 & 8

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Functions:
+        create_RMSProp_op(loss, alpha, beta2, epsilon).
+"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """ Function that updates a variable using the RMSProp
+        optimization algorithm.
+
+    Args:
+        loss (): is the loss of the network.
+        alpha (): is the learning rate.
+        beta2 (): is the RMSProp weight.
+        epsilon (): is a small number to avoid division by zero.
+
+    Return:
+        The RMSProp optimization operation.
+    """
+    train = tf.train.RMSPropOptimizer(learning_rate=alpha,
+                                      decay=beta2,
+                                      epsilon=epsilon)
+    return train.minimize(loss)

bac26ca4d6749e08062fd62232234bcef94300dd
juan8bits <juan160820gmail.com>
2021-09-04 13:21:37 -0500

    Fix pycodestyle

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -10,17 +10,20 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
         decay in numpy.
     Args:
         alpha (): is the original learning rate.
-        decay_rate (): is the weight used to find the rate at which α will decay.
-        global_step (): is the # of passes of gradient descent that have elapsed.
-        decay_step (): is the number of passes of gradient descent that should
+        decay_rate (): is the weight used to find the rate at which
+            a will decay.
+        global_step (): is the # of passes of gradient descent that
+            have elapsed.
+        decay_step (): is the number of passes of gradient descent
+            that should
             occur before alpha is decayed further.
 
     Returns:
         The updated value for alpha.
     """
     inverse_train = tf.train.inverse_time_decay(learning_rate=alpha,
-                                     global_step=global_step,
-                                     decay_steps=decay_step,
-                                     decay_rate=decay_rate,
-                                     staircase=True)
+                                                global_step=global_step,
+                                                decay_steps=decay_step,
+                                                decay_rate=decay_rate,
+                                                staircase=True)
     return inverse_train

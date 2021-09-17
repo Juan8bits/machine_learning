ca868040830f1d8dd6eca371e979e376d87744d3
juan8bits <juan160820gmail.com>
2021-09-16 19:26:23 -0500

    Add task 5

---
+++ supervised_learning/0x08-deep_cnns/5-dense_block.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" Functions:
+        dense_block(X, nb_filters, growth_rate, layers).
+"""
+import tensorflow.keras as K
+
+
+def dense_block(X, nb_filters, growth_rate, layers):
+    """ Function that builds a dense block.
+
+    Args:
+        X (): output from the previous layer
+        nb_filters (): integer representing the number of filters in X
+        growth_rate (): growth rate for the dense block
+        layers (): number of layers in the dense block
+
+    Return: concatenated output of each layer within the Dense Block and the
+            number of filters within the concatenated outputs.
+    """
+    # He et. al initialization for the layers weights
+    initializer = K.initializers.he_normal(seed=None)
+
+    for i in range(layers):
+        my_layer = K.layers.BatchNormalization()(X)
+        my_layer = K.layers.Activation('relu')(my_layer)
+
+        # conv 1×1 produces 4k (growth_rate) feature-maps
+        my_layer = K.layers.Conv2D(filters=4*growth_rate,
+                                   kernel_size=1,
+                                   padding='same',
+                                   kernel_initializer=initializer,
+                                   )(my_layer)
+
+        my_layer = K.layers.BatchNormalization()(my_layer)
+        my_layer = K.layers.Activation('relu')(my_layer)
+
+        # conv 3×3 produces k (growth_rate) feature-maps
+        my_layer = K.layers.Conv2D(filters=growth_rate,
+                                   kernel_size=3,
+                                   padding='same',
+                                   kernel_initializer=initializer,
+                                   )(my_layer)
+
+        X = K.layers.concatenate([X, my_layer])
+        nb_filters += growth_rate
+
+    return X, nb_filters

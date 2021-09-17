c43f9ae559d8a922c0f17015dbf65e457f2e7201
juan8bits <juan160820gmail.com>
2021-09-16 19:16:27 -0500

    Move the content of the file by the right content

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
-""" Functions:
-        projection_block(A_prev, filters, s=2)
+"""
+    Deep Convolutional Architectures
 """
 import tensorflow.keras as K
 
 
-def projection_block(A_prev, filters, s=2):
-    """ Function that builds a projection block for resnet
-
-    Args:
-        A_prev (): is the output from previous layer.
-        filters (): is tuple/list (F11, F3, F12).
-            F11 first 1x1 conv.
-            F3 3x3 conv.
-            F12 second 1x1 conv.
-    Returns: activated output of block
+def identity_block(A_prev, filters):
+    """ Builds an identity block for resnet
+        A_prev is output from previous layer
+        filters is tuple/list (F11, F3, F12)
+            F11 first 1x1 conv
+            F3 3x3 conv
+            F12 second 1x1 conv
+        Batch norm after each convolution,
+            along channels axis and ReLu
+        Returns: activated output of block
     """
     init = K.initializers.he_normal()
     F11, F3, F12 = filters
     conv = K.layers.Conv2D(F11, kernel_size=(1, 1),
-                           strides=s,
+                           strides=(1, 1),
                            padding='valid',
                            kernel_initializer=init)(A_prev)
     bnorm = K.layers.BatchNormalization()(conv)
@@ -36,15 +36,9 @@ def projection_block(A_prev, filters, s=2):
                            strides=(1, 1),
                            padding='valid',
                            kernel_initializer=init)(relu)
-    last = K.layers.BatchNormalization()(conv)
+    bnorm = K.layers.BatchNormalization()(conv)
 
-    shortcut = K.layers.Conv2D(F12, kernel_size=(1, 1),
-                               strides=s,
-                               padding='valid',
-                               kernel_initializer=init)(A_prev)
-    shortcut = K.layers.BatchNormalization()(shortcut)
-
-    relu = K.layers.Add()([last, shortcut])
-    out = K.layers.Activation('relu')(relu)
+    shortcut = K.layers.Add()([bnorm, A_prev])
+    out = K.layers.Activation('relu')(shortcut)
 
     return out

2c0d7854dc0b21346e7e1848e13ed9beec795c41
juan8bits <juan160820gmail.com>
2022-03-19 22:25:23 -0500

    add task 4 inverse

---
+++ math/0x05-advanced_linear_algebra/4-inverse.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+""" 4-inverse
+"""
+
+
+def determinant(matrix):
+    """ Calculates the determinant of a matrix
+        matrix is a square list of lists whose determinant should be calculated
+        Returns: the determinant of matrix
+    """
+    if matrix == [[]]:
+        return 1
+    if type(matrix) is not list or len(matrix) < 1 or\
+            not all(isinstance(x, list) for x in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if not all(len(matrix) == len(x) for x in matrix):
+        raise ValueError("matrix must be a square matrix")
+    copy = list(map(list, matrix))
+    dim = len(matrix)
+    if dim == 1:
+        return matrix[0][0]
+    elif dim == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+    else:
+        for cur in range(dim):
+            for i in range(cur + 1, dim):
+                if copy[cur][cur] == 0:
+                    copy[cur][cur] = 1.0e-10
+                curScaler = copy[i][cur] / copy[cur][cur]
+                for j in range(dim):
+                    copy[i][j] = copy[i][j] - curScaler * copy[cur][j]
+        det = 1
+        for i in range(dim):
+            det *= copy[i][i]
+    return round(det)
+
+
+def cofactor(matrix):
+    """ Calculates the matrix of minors
+        matrix is a square list of lists whose determinant should be calculated
+        Returns: the matrix of minors
+    """
+    if type(matrix) is not list or len(matrix) < 1 or\
+            not all(isinstance(x, list) for x in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if not all(len(matrix) == len(x) for x in matrix) or matrix == [[]]:
+        raise ValueError("matrix must be a non-empty square matrix")
+    copy = list(map(list, matrix))
+    dim = len(matrix)
+    if dim == 1:
+        return [[1]]
+    elif dim == 2:
+        return [[matrix[1][1], -matrix[1][0]], [-matrix[0][1], matrix[0][0]]]
+    else:
+        minors = []
+        mins = []
+        for i in range(dim):
+            for j in range(dim):
+                minor = matrix[:i] + matrix[i + 1:]
+                for k in range(len(minor)):
+                    minor[k] = minor[k][: j] + minor[k][j + 1:]
+                minors.append(minor)
+            mins.append(minor)
+        mm = []
+        mmm = []
+        for m in range(len(minors)):
+            co = 1 if ((m + 1) % 2) else -1
+            mm.append(co * determinant(minors[m]))
+            if (m + 1) % dim == 0:
+                mmm.append(mm)
+                mm = []
+        return mmm
+
+
+def adjugate(matrix):
+    """ basically just transpose the minor matrix """
+    mm = cofactor(matrix)
+    adj = list(zip(*mm))
+    adj = [list(x) for x in adj]
+    return adj
+
+
+def inverse(matrix):
+    """ calculates the inverse
+        basically multiply (adjucate) * (1 / determinant)
+        Returns: the inverse of matrix, or None if matrix is singular
+    """
+    adj = adjugate(matrix)
+    det = determinant(matrix)
+    if det == 0:
+        return None
+    dim = len(matrix)
+    inverse = list(map(list, matrix))
+    for i in range(dim):
+        for j in range(dim):
+            inverse[i][j] = (adj[i][j]) / det
+    return inverse

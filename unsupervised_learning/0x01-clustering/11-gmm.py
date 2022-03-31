15587a14ca8b902e87b9a1917e1b0d06bb218064
juan8bits <juan160820gmail.com>
2022-03-31 18:26:16 -0500

    pycodestyle fix

--- unsupervised_learning/0x01-clustering/11-gmm.py
+++ unsupervised_learning/0x01-clustering/11-gmm.py
@@ -14,10 +14,14 @@ def gmm(X, k):
         Returns:
             - pi, m, S, clss, bic
             -pi is a numpy.ndarray of shape (k,) containing the cluster priors
-            - m is a numpy.ndarray of shape (k, d) containing the centroid means
-            - S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices
-            - clss is a numpy.ndarray of shape (n,) containing the cluster indices for each data point
-            - bic is a numpy.ndarray of shape (kmax - kmin + 1) containing the BIC value for each cluster size tested
+            - m is a numpy.ndarray of shape (k, d) containing the
+                centroid means
+            - S is a numpy.ndarray of shape (k, d, d) containing the
+                covariance matrices
+            - clss is a numpy.ndarray of shape (n,) containing the
+                cluster indices for each data point
+            - bic is a numpy.ndarray of shape (kmax - kmin + 1) containing
+                the BIC value for each cluster size tested
     """
     gm = sklearn.mixture.GaussianMixture(k).fit(X)
 

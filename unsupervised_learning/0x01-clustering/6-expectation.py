91ec754aa37cc5fa7211e02b7eb79df22fb53e6f
juan8bits <juan160820gmail.com>
2022-03-31 18:20:01 -0500

    add task 6

---
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+    Functions:
+        def expectation(X, pi, m, S)
+"""
+import numpy as np
+pdf = __import__('5-pdf').pdf
+
+
+def expectation(X, pi, m, S):
+    """ expectation step in the EM algorithm for a GMM
+        Args:
+            X: ndarray (n, d) dataset
+            pi: ndarray (k,) priors for each cluster
+            m: ndarray (k, d) centroid means
+            S: ndarray (k, d, d) covariance of distribution
+        Returns:
+            - g, l, or None, None on failure
+            - g ndarray (k, n) containing posterior probabilities
+            - l total log likelihood
+    """
+    if type(X) is not np.ndarray or X.ndim != 2\
+            or type(pi) is not np.ndarray or pi.ndim != 1\
+            or type(m) is not np.ndarray or m.ndim != 2\
+            or type(S) is not np.ndarray or S.ndim != 3:
+        return None, None
+    n, d = X.shape
+    k = pi.shape[0]
+    g = []
+    for j in range(k):
+        p = pdf(X, m[j], S[j]) * pi[j]
+        g.append(p)
+    prob = np.array(g)
+    hood = prob.sum(axis=0)
+    prob = prob / hood
+    loghood = np.sum(np.log(hood))
+    return prob, loghood
